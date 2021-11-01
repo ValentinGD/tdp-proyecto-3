705736c79ba79b5b5ac5a8b3ee4c57708e6253a2
Lucas Schmit <l.schmit.2000@gmail.com>
2021-11-01 18:50:37 -0300

    mapa1 creado

--- src/logica/niveles/Nivel.java
+++ src/logica/niveles/Nivel.java
@@ -32,6 +32,7 @@ public class Nivel {
 	public void cargarGrilla() throws IOException {
 		FileReader fileReader = new FileReader(pathMapa);
 	    int caracterLeido = fileReader.read();
+	    
 	    char caracter;
 		for(int f=0;f<grilla[0].length;f++) {
 			for(int c=0;c<grilla.length;c++) {
