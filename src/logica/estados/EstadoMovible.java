b6158eef3367c594a007c9b84e0f7d58e7615669
Lucas Schmit <l.schmit.2000@gmail.com>
2021-11-15 18:07:19 -0300

    Cambio EstadoMovible

--- src/logica/estados/EstadoMovible.java
+++ src/logica/estados/EstadoMovible.java
@@ -2,9 +2,13 @@ package logica.estados;
 
 import javax.swing.ImageIcon;
 
+import logica.entidades.Pared;
+
 public interface EstadoMovible {
 	
 	public void mover();
 	
 	public boolean puedeMover();
+	
+	public void visitarPared(Pared Pared);
 }
