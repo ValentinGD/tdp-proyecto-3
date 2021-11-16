0d42a5e5d236cb1f799168e999b53ee8d716ff56
ValentinGD <valenelpala@hotmail.com>
2021-11-16 13:49:22 -0300

    Update PersiguiendoEnemigo1.java

--- src/logica/estados/enemigos/PersiguiendoEnemigo1.java
+++ src/logica/estados/enemigos/PersiguiendoEnemigo1.java
@@ -52,62 +52,112 @@ public class PersiguiendoEnemigo1 extends PersiguiendoEnemigoAbstracto {
 	}
 	
 	private void calcularNuevaDireccion() {
-		int direccionEjeX=0;
-		int direccionEjeY = 0;
 		Personaje personaje = Personaje.getInstancia();
 		
+		int direccionEjeX=0;
+		int direccionEjeY = 0;
+		int mejorDireccion=0;
+		int direccionAux=0;
+		int nuevaDireccion=0;
+		int diferenciaEnX=enemigo.getX()-personaje.getX();
+		int diferenciaEnY=enemigo.getY()-personaje.getY();
 		
-		if(enemigo.getX()-personaje.getX()<0) {
+		if(diferenciaEnX<0) {
 			direccionEjeX=Movible.DIRECCION_DERECHA;
 		}else {
 			direccionEjeX=Movible.DIRECCION_IZQUIERDA;
 		}
 		
-		if(enemigo.getY()-personaje.getY()<0) {
+		if(diferenciaEnY<0) {
 			direccionEjeY=Movible.DIRECCION_ABAJO;
 		}else {
 			direccionEjeY=Movible.DIRECCION_ARRIBA;
 		}
-		int diferenciaEnX=Math.abs(enemigo.getX()-personaje.getX());
-		int diferenciaEnY=Math.abs(enemigo.getY()-personaje.getY());
-		
-		if(diferenciaEnX<diferenciaEnY && diferenciaEnX!= 0) {
-			if((Movible.sonDireccionesOpuestas(direccionActual, direccionEjeX) && (puedeMover(enemigo, direccionEjeX)))){
-				setDireccion(direccionEjeX);
-				enemigo.setDireccionActual(direccionEjeX);
+		if(Math.min(diferenciaEnX, diferenciaEnY)== diferenciaEnX ){
+			if(diferenciaEnX!=0) {
+				mejorDireccion=direccionEjeX;
+				direccionAux=direccionEjeY;
 			}else {
-				if((Movible.sonDireccionesOpuestas(direccionActual, direccionEjeY) && (puedeMover(enemigo, direccionEjeY)))) {
-					setDireccion(direccionEjeY);
-					enemigo.setDireccionActual(direccionEjeY);
-				}else {
-						if(Movible.sonDireccionesOpuestas(direccionActual, direccionEjeX*(-1)) && (puedeMover(enemigo, direccionEjeX))){
-							setDireccion(direccionEjeX*(-1));
-							enemigo.setDireccionActual(direccionEjeX*(-1));
-						}else {
-							setDireccion(direccionEjeY*(-1));
-							enemigo.setDireccionActual(direccionEjeY*(-1));
-						}
-				}
+				mejorDireccion=direccionEjeY;
+				direccionAux=direccionEjeX;
 			}
 		}else {
-			if((Movible.sonDireccionesOpuestas(direccionActual, direccionEjeY) && (puedeMover(enemigo, direccionEjeY)))) {
-				setDireccion(direccionEjeY);
-				enemigo.setDireccionActual(direccionEjeY);
+			if(diferenciaEnY!=0) {
+				mejorDireccion=direccionEjeY;
+				direccionAux=direccionEjeX;
 			}else {
-				if((Movible.sonDireccionesOpuestas(direccionActual, direccionEjeX) && (puedeMover(enemigo, enemigo.getDireccionActual())))){
-					setDireccion(direccionEjeX);
-					enemigo.setDireccionActual(direccionEjeX);
-				}else {
-					if((Movible.sonDireccionesOpuestas(direccionActual, direccionEjeY*(-1)) && (puedeMover(enemigo, direccionEjeY*(-1))))) {
-						setDireccion(direccionEjeY*(-1));
-						enemigo.setDireccionActual(direccionEjeY*(-1));
-					}else {
-					setDireccion(direccionEjeX*(-1));
-					enemigo.setDireccionActual(direccionEjeX*(-1));
-					}
+				mejorDireccion=direccionEjeX;
+				direccionAux=direccionEjeY;
 				}
+		}
+		direccionActual=calcularDireccion(mejorDireccion,direccionAux);
+		enemigo.setDireccionActual(calcularDireccion(mejorDireccion,direccionAux));
+	}
+	
+	private int calcularDireccion(int direccion,int direccionAux) {
+		int nuevaDireccion=0;
+		int direccion1ra=0;
+		int direccion2da=0;
+		int direccion3ra=0;
+		int marcador=0;
+		if(Movible.sonDireccionesOpuestas(direccion, direccionActual)) {
+			marcador=+1;
+		}
+		if(Movible.sonDireccionesOpuestas(direccionAux, direccionActual)) {
+			marcador=+2;
+		}
+		if(Movible.sonDireccionesOpuestas(direccion*(-1), direccionActual)) {
+			marcador=+3;
+		}
+		switch(marcador) {
+		case 0:
+			
+			direccion1ra=direccion;
+			direccion2da=direccionAux;
+			direccion3ra=direccion*(-1);
+			
+		break;
+		case 1:
+			
+			direccion1ra=direccionAux;
+			direccion2da=direccion*(-1);
+			direccion3ra=direccionAux*(-1);
+			
+		break;
+		case 2:
+	
+			direccion1ra=direccion;
+			direccion2da=direccion*(-1);
+			direccion3ra=direccionAux*(-1);
+	
+		break;
+		case 3:
+	
+			direccion1ra=direccion;
+			direccion2da=direccionAux;
+			direccion3ra=direccionAux*(-1);
+	
+			break;
+		}
+	nuevaDireccion=verificarDireccion(direccion1ra,direccion2da,direccion3ra);
+	return nuevaDireccion;
+	}
+	
+	private int verificarDireccion(int d1, int d2, int d3) {
+		int nuevaDireccion=0;
+		if(puedeMover(enemigo,d1)){
+			nuevaDireccion=d1;
+			System.out.println("Puede mover d1");
+		}else{
+			if(puedeMover(enemigo,d2)) {
+				nuevaDireccion=d2;
+				System.out.println("Puede mover d2");
+			}else {
+				nuevaDireccion=d3;
+				System.out.println("Puede mover d3");
 			}
 		}
+		return nuevaDireccion;
 	}
 	private boolean unicaDireccion() {
 		boolean d1,d2,d3,d4;
