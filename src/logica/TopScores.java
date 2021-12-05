9703aafb9f92845381c92187b0ea03f9240c3e5f
Dario A. Leal <l.dario.a@gmail.com>
2021-12-04 22:27:30 -0300

    corregido top scores
    porque guardaba null's o vacíos.

--- src/logica/TopScores.java
+++ src/logica/TopScores.java
@@ -16,34 +16,31 @@ public class TopScores implements Serializable{
 	}
 	
 	/*
-	 * Si es un puntaje que entra dentro de los 10 mejores, solicita el nombre del jugador y luego guarda nombre y puntaje en la lista.
+	 * Si es un puntaje que entra dentro de los 10 mejores, se guardara en el listado.
 	 * El retorno indica si la lista fue efectivamente actualizada con un nuevo record.
 	 */
 	public boolean procesarPuntaje(int puntaje_nuevo) {
 		boolean se_guardo_record = false;
-		String nombre_jugador;
 		if (!ranking.isEmpty()) {
 			boolean enconteMenor = false;
 			int puntaje_aux;
 			for (int i = 0; i < ranking.size() && !enconteMenor; i++) {
 				puntaje_aux = ranking.get(i).getPuntaje();
 				if (puntaje_aux < puntaje_nuevo) {
-					nombre_jugador = solicitarNombre();
-					ranking.add(i, new Player(nombre_jugador, puntaje_nuevo));
+					guardarEnLista(i, puntaje_nuevo);
 					enconteMenor = true;
 					se_guardo_record = true;
 				}
 			}
 			if (!enconteMenor && ranking.size() < 10) {
-				nombre_jugador = solicitarNombre();
-				ranking.add(new Player(nombre_jugador, puntaje_nuevo));
+				guardarEnLista(-1, puntaje_nuevo);
 				se_guardo_record = true;
 			}
-			if (ranking.size() > 10)
+			if (ranking.size() > 10) {
 				ranking.remove(ranking.size() - 1);
+			}
 		} else {
-			nombre_jugador = solicitarNombre();
-			ranking.add(new Player(nombre_jugador, puntaje_nuevo));
+			guardarEnLista(-1, puntaje_nuevo);
 			se_guardo_record = true;
 		}
 		return se_guardo_record;
@@ -52,6 +49,22 @@ public class TopScores implements Serializable{
 	private String solicitarNombre() {
 		NuevoRecord nuevo_record = new NuevoRecord();
 		return nuevo_record.cartel_solicitar_nombre();
+	}
+	
+	/*
+	 * Solicita el nombre del jugador y luego lo guarda en la lista junto con el puntaje.
+	 * Si pos = -1 guarda al final de la lista, caso contrario guarda en la posición pos. 
+	 */
+	private void guardarEnLista(int pos, int puntaje) {
+		String nombre = solicitarNombre();
+		if (nombre != null && !nombre.isBlank()) {
+			if (pos == -1) {
+				ranking.add(new Player(nombre, puntaje));
+			}
+			else {
+				ranking.add(pos, new Player(nombre, puntaje));
+			}
+		}
 	}
 	
 	public List<Player> getPuntajes() {
